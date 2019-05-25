4e9186ff148d692780534c0f0e17c1b9746392d1
Austin Kregel <austin@kbcomp.co>
2019-05-25 19:58:08 -0400

    Rework the LaravelAbstract class and have it be a singleton now instead of a static config

--- src/LaravelAbstract.php
+++ src/LaravelAbstract.php
@@ -7,27 +7,63 @@ use Closure;
 class LaravelAbstract
 {
     /**
+     * The instace we use across the app
+     * @var null|static
+     */
+    private static $instance = null;
+    /**
      * The middleware applied to the routes
      * @var string|array
      */
-    public static $middlewareGroup = 'web';
-
+    public $middlewareGroup = 'web';
     /**
      * Whether or not we should bypass the policies
      * @var bool
      */
-    public static $bypassPolicies = false;
+    public $bypassPolicies = false;
+    /**
+     * The model-key => Model bindings for the dynamic routes.
+     * @var array
+     */
+    public $routeBindings = [];
+    /**
+     * The indicator of whether or not we should declare the routes in the file.
+     * @var bool
+     */
+    public $usingRoutes = true;
+    /**
+     * The way we resolve the abstract model
+     * @var Closure
+     */
+    public $resolveModelsUsing;
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return LaravelAbstract
+     */
+    public static function bind(): LaravelAbstract
+    {
+        if (static::$instance == null) {
+            static::$instance = new static;
+        }
+
+        return static::$instance;
+    }
+
 
     /**
      * Set the specific the middleware that will be applied to the abstract routes.
-     * @param string|array $middleware
+     * @param  string|array  $middleware
      * @return static
      */
-    public static function middleware($middleware)
+    public function middleware($middleware): LaravelAbstract
     {
-        static::$middlewareGroup = $middleware;
+        $this->middlewareGroup = $middleware;
 
-        return new static;
+        return $this;
     }
 
     /**
@@ -35,10 +71,49 @@ class LaravelAbstract
      * @param  bool  $bypass
      * @return LaravelAbstract
      */
-    public static function bypass(bool $bypass = false)
+    public function bypass(bool $bypass = false): LaravelAbstract
     {
-        static::$bypassPolicies = $bypass;
+        $this->bypassPolicies = $bypass;
 
-        return new static;
+        return $this;
+    }
+
+    /**
+     * @param  array|string  $modelOrModels
+     * @return string|static
+     */
+    public function route($modelOrModels = [])
+    {
+        if (is_string($modelOrModels)) {
+            return $this->routeBindings[$modelOrModels];
+        }
+
+        $this->routeBindings = $modelOrModels;
+
+        return $this;
+    }
+
+    /**
+     * Set whether or not we will use the abstracted routes.
+     * @param  bool  $useRoutes
+     * @return LaravelAbstract
+     */
+    public function useRoutes(bool $useRoutes = false): LaravelAbstract
+    {
+        $this->usingRoutes = $useRoutes;
+
+        return $this;
+    }
+
+    /**
+     * Change the way the package tries to resolve the eloquent models from the URI
+     * @param  Closure  $closure
+     * @return LaravelAbstract
+     */
+    public function resolveUsing(Closure $closure): LaravelAbstract
+    {
+        $this->resolveModelsUsing = $closure;
+
+        return $this;
     }
 }
