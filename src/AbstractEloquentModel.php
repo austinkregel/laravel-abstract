ed977e016d2ccbba704f27492af8ca7cc8d2533c
Austin Kregel <austin@kbcomp.co>
2019-07-14 22:08:52 -0400

    Turn the eloquent model into an interface... It still needs to be an eloquent model tho

--- src/AbstractEloquentModel.php
+++ src/AbstractEloquentModel.php
@@ -2,32 +2,13 @@
 
 namespace Kregel\LaravelAbstract;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-
-abstract class AbstractEloquentModel extends Model
+interface AbstractEloquentModel
 {
     public const VALIDATION_CREATE_RULES = [];
     public const VALIDATION_UPDATE_RULES = [];
-
-    public function scopeQ(Builder $query, string $string)
-    {
-        $fields = $this->getAbstractSearchableFields();
-
-        foreach ($fields as $field) {
-            $query->orWhere($field, 'like', '%'.$string.'%');
-        }
-    }
-
-    public function usesSoftdeletes()
-    {
-        return in_array(SoftDeletes::class, class_uses($this));
-    }
-
-    abstract public function getAbstractAllowedFilters(): array;
-    abstract public function getAbstractAllowedRelationships(): array;
-    abstract public function getAbstractAllowedSorts(): array;
-    abstract public function getAbstractAllowedFields(): array;
-    abstract public function getAbstractSearchableFields(): array;
+    public function getAbstractAllowedFilters(): array;
+    public function getAbstractAllowedRelationships(): array;
+    public function getAbstractAllowedSorts(): array;
+    public function getAbstractAllowedFields(): array;
+    public function getAbstractSearchableFields(): array;
 }
